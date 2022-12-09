0f1a02cd3e4f565da380a6a5108d696dc9bec6fa
Chaaru Dingankar <chaaru@Chaarus-MacBook-Pro.local>
2022-12-08 20:59:08 -0800

    update setup script

--- new.sh
+++ new.sh
@@ -1,3 +1,5 @@
+# should be run as source ./new.sh abc to successfully cd in the terminal process
+
 if [ -z "$1" ]; then
   echo "You must provide a directory name to set up"
   exit
@@ -5,4 +7,5 @@ fi
 
 mkdir "$1"
 cp template.scala "$1"/"$1".scala
+touch "$1"/input
 cd "$1" || exit
